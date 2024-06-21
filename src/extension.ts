21af312b46645f83da2b07c585502cb9de36d692
zax4r0 <zax4r0@gmail.com>
2024-06-21 18:37:11 +0530

    fix: :bug: fix tree view

--- src/extension.ts
+++ src/extension.ts
@@ -3,14 +3,13 @@
 import * as vscode from 'vscode';
 import setupNotes from './setup-notes';
 import listNotes from './list-notes';
-import VSNotesTreeView from './tree-view';
-import { getDefaultNotePath } from './utils';
+import VSNotesTreeView, { TreeNode } from './tree-view';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	const tv = new VSNotesTreeView();
-	vscode.window.registerTreeDataProvider('vsnotes', tv);
+	const treeViewProvider = new VSNotesTreeView();
+	vscode.window.registerTreeDataProvider('vsnotes', treeViewProvider);
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vsnotes" is now active!');
@@ -20,6 +19,12 @@ export function activate(context: vscode.ExtensionContext) {
 	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('vsnotes.setupNotes', setupNotes);
 	context.subscriptions.push(disposable);
+
+	// Refresh View
+	vscode.commands.registerCommand('vsnotes.refreshVSNotesView', () => treeViewProvider.refresh());
+
+	vscode.commands.registerCommand('vsnotes.renameNote', (node: TreeNode) => treeViewProvider.renameNode(node));
+	vscode.commands.registerCommand('vsnotes.deleteNote', (node: TreeNode) => treeViewProvider.deleteNode(node));
 
 	const listNotesDisposable = vscode.commands.registerCommand('vsnotes.listNotes', listNotes);
 	context.subscriptions.push(listNotesDisposable);
--- src/tree-view.ts
+++ src/tree-view.ts
@@ -1,202 +1,242 @@
 import * as vscode from 'vscode';
-import fs from 'fs';
+import fs from 'fs-extra';
 import path from 'path';
 import klaw from 'klaw';
 import matter from 'gray-matter';
 import { getDefaultNotePath } from './utils';
 
-class VSNotesTreeView implements vscode.TreeDataProvider<TreeNode> {
-	private _onDidChangeTreeData: vscode.EventEmitter<TreeNode | undefined> = new vscode.EventEmitter<
-		TreeNode | undefined
-	>();
-	readonly onDidChangeTreeData: vscode.Event<TreeNode | undefined> = this._onDidChangeTreeData.event;
+export interface TreeNode {
+	type: 'rootTag' | 'tag' | 'rootFile' | 'file';
+	tag?: string;
+	files?: TreeNode[];
+	file?: string;
+	path?: string;
+	stats?: fs.Stats;
+}
 
+class VSNotesTreeView {
 	private baseDir: string;
 	private ignorePattern: RegExp;
 	private hideTags: boolean;
 	private hideFiles: boolean;
+	private _onDidChangeTreeData: vscode.EventEmitter<void>;
+	public onDidChangeTreeData: vscode.Event<void>;
 
 	constructor() {
 		const config = vscode.workspace.getConfiguration('vsnotes');
 		this.baseDir = getDefaultNotePath();
 		this.ignorePattern = new RegExp(
-			config
-				.get('ignorePatterns')
-				.map((pattern: string) => `(${pattern})`)
-				.join('|')
+			(config.get<string[]>('ignorePatterns') || []).map((pattern) => `(${pattern})`).join('|')
 		);
-		this.hideTags = config.get('treeviewHideTags');
-		this.hideFiles = config.get('treeviewHideFiles');
+		this.hideTags = config.get<boolean>('treeviewHideTags') || false;
+		this.hideFiles = config.get<boolean>('treeviewHideFiles') || false;
+		this._onDidChangeTreeData = new vscode.EventEmitter();
+		this.onDidChangeTreeData = this._onDidChangeTreeData.event;
 	}
 
 	refresh(): void {
 		this._onDidChangeTreeData.fire();
 	}
 
-	getTreeItem(element: TreeNode): vscode.TreeItem {
-		return element.treeItem;
-	}
-
-	async getChildren(element?: TreeNode): Promise<TreeNode[]> {
-		if (!element) {
+	async getChildren(node?: TreeNode): Promise<TreeNode[] | undefined> {
+		if (node) {
+			vscode.window.showInformationMessage(`Node type: ${node.type}`);
+			switch (node.type) {
+				case 'rootTag':
+					return this._getTags();
+				case 'tag':
+					return node.files;
+				case 'rootFile':
+					return this._getDirectoryContents(this.baseDir);
+				case 'file':
+					return this._getDirectoryContents(node.path!);
+			}
+		} else {
 			const treeview: TreeNode[] = [];
 			if (!this.hideFiles) {
-				treeview.push(new TreeNode('Files', vscode.TreeItemCollapsibleState.Expanded, 'file-directory.svg'));
+				treeview.push({ type: 'rootFile' });
 			}
 			if (!this.hideTags) {
-				treeview.push(new TreeNode('Tags', vscode.TreeItemCollapsibleState.Expanded, 'tag.svg'));
+				treeview.push({ type: 'rootTag' });
 			}
 			return treeview;
 		}
-
-		switch (element.type) {
-			case 'rootTag':
-				return this.getTags();
-			case 'tag':
-				return element.files.map(
-					(file) => new TreeNode(file, vscode.TreeItemCollapsibleState.None, 'file.svg')
-				);
-			case 'rootFile':
-				return this.getDirectoryContents(this.baseDir);
-			case 'file':
-				return this.getDirectoryContents(element.path);
-			default:
-				return [];
-		}
 	}
 
-	private async getDirectoryContents(filePath: string): Promise<TreeNode[]> {
-		const files = await fs.readdir(filePath);
-		const items: TreeNode[] = [];
-		for (const file of files) {
-			if (!this.ignorePattern.test(file)) {
-				const stats = await fs.stat(path.join(filePath, file));
-				items.push(
-					new TreeNode(
-						file,
-						stats.isDirectory()
-							? vscode.TreeItemCollapsibleState.Collapsed
-							: vscode.TreeItemCollapsibleState.None,
-						'file.svg',
-						filePath
-					)
-				);
+	getTreeItem(node: TreeNode): vscode.TreeItem {
+		switch (node.type) {
+			case 'rootTag': {
+				const item = new vscode.TreeItem('Tags', vscode.TreeItemCollapsibleState.Expanded);
+				item.contextValue = node.type;
+				item.iconPath = {
+					light: path.join(__filename, '..', '..', 'media', 'light', 'tag.svg'),
+					dark: path.join(__filename, '..', '..', 'media', 'dark', 'tag.svg'),
+				};
+				return item;
+			}
+			case 'rootFile': {
+				const item = new vscode.TreeItem('Files', vscode.TreeItemCollapsibleState.Expanded);
+				item.contextValue = node.type;
+				item.iconPath = {
+					light: path.join(__filename, '..', '..', 'media', 'light', 'file-directory.svg'),
+					dark: path.join(__filename, '..', '..', 'media', 'dark', 'file-directory.svg'),
+				};
+				return item;
+			}
+			case 'tag': {
+				const item = new vscode.TreeItem(node.tag!, vscode.TreeItemCollapsibleState.Collapsed);
+				item.contextValue = node.type;
+				item.iconPath = {
+					light: path.join(__filename, '..', '..', 'media', 'light', 'tag.svg'),
+					dark: path.join(__filename, '..', '..', 'media', 'dark', 'tag.svg'),
+				};
+				return item;
+			}
+			case 'file': {
+				const isDir = node.stats!.isDirectory();
+				const state = isDir ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None;
+				const item = new vscode.TreeItem(node.file!, state);
+				item.contextValue = node.type;
+				if (isDir) {
+					item.iconPath = {
+						light: path.join(__filename, '..', '..', 'media', 'light', 'file-directory.svg'),
+						dark: path.join(__filename, '..', '..', 'media', 'dark', 'file-directory.svg'),
+					};
+				} else {
+					item.command = {
+						command: 'vscode.open',
+						title: '',
+						arguments: [vscode.Uri.file(node.path!)],
+					};
+					item.iconPath = {
+						light: path.join(__filename, '..', '..', 'media', 'light', 'file.svg'),
+						dark: path.join(__filename, '..', '..', 'media', 'dark', 'file.svg'),
+					};
+				}
+				return item;
 			}
 		}
-		return items;
 	}
 
-	private async getTags(): Promise<TreeNode[]> {
-		const files: Promise<{ path: string; contents: Buffer; payload: TreeNode }>[] = [];
+	async renameNode(node: TreeNode): Promise<void> {
+		const newName = await vscode.window.showInputBox({
+			placeHolder: 'Enter the new name',
+			value: node.file,
+		});
+		if (newName) {
+			const newPath = path.join(path.dirname(node.path!), newName);
+			await fs.rename(node.path!, newPath);
+			this.refresh();
+		}
+	}
 
-		klaw(this.baseDir)
-			.on('data', (item) => {
-				files.push(
-					new Promise((resolve, reject) => {
-						const fileName = path.basename(item.path);
-						if (!item.stats.isDirectory() && !this.ignorePattern.test(fileName)) {
-							fs.readFile(item.path)
-								.then((contents) => {
-									resolve({
-										path: item.path,
-										contents: contents,
-										payload: new TreeNode(
-											fileName,
-											vscode.TreeItemCollapsibleState.None,
-											'file.svg',
-											item.path
-										),
-									});
-								})
-								.catch((err) => {
-									console.error(err);
-									resolve();
-								});
-						} else {
-							resolve();
-						}
+	async deleteNode(node: TreeNode): Promise<void> {
+		const confirmation = await vscode.window.showWarningMessage(
+			`Are you sure you want to delete ${node.file}?`,
+			{ modal: true },
+			'Yes'
+		);
+		if (confirmation === 'Yes') {
+			await fs.remove(node.path!);
+			this.refresh();
+		}
+	}
+
+	private async _getDirectoryContents(filePath: string): Promise<TreeNode[]> {
+		try {
+			const files = await fs.readdir(filePath);
+			const items = await Promise.all(
+				files
+					// .filter((file) => !this.ignorePattern.test(file))
+					.map(async (file) => {
+						const filePathFull = path.join(filePath, file);
+						const stats = await fs.stat(filePathFull);
+						return { type: 'file', file, path: filePathFull, stats };
 					})
-				);
-			})
-			.on('error', (err, item) => {
-				console.error('Error while walking notes folder for tags: ', item, err);
-			});
+			);
+			return items as TreeNode[];
+		} catch (err) {
+			console.error('Error reading directory contents:', filePath, err);
+			return [];
+		}
+	}
 
-		const filesResult = await Promise.all(files);
-		const tagIndex: { [tag: string]: TreeNode[] } = {};
-		filesResult.forEach((file) => {
-			if (file) {
-				const parsedFrontMatter = this.parseFrontMatter(file);
-				if (parsedFrontMatter && 'tags' in parsedFrontMatter.data && parsedFrontMatter.data.tags) {
-					for (const tag of parsedFrontMatter.data.tags) {
-						if (tag in tagIndex) {
-							tagIndex[tag].push(file.payload);
-						} else {
-							tagIndex[tag] = [file.payload];
+	private async _getTags(): Promise<TreeNode[]> {
+		const files: Promise<{ path: string; contents: string; payload: TreeNode } | undefined>[] = [];
+		return new Promise((resolve, reject) => {
+			klaw(this.baseDir)
+				.on('data', (item) => {
+					const fileName = path.basename(item.path);
+					if (!item.stats.isDirectory() && !this.ignorePattern.test(fileName)) {
+						files.push(
+							fs
+								.readFile(item.path, 'utf8')
+								.then((contents) => ({
+									path: item.path,
+									contents,
+									payload: {
+										type: 'file' as 'file', // Explicitly setting the type
+										file: fileName,
+										path: item.path,
+										stats: item.stats,
+									} as TreeNode, // Casting to TreeNode
+								}))
+								.catch((err) => {
+									console.error('Error reading file:', item.path, err);
+									return undefined;
+								})
+						);
+					}
+				})
+				.on('error', (err: any, item: klaw.Item) => {
+					reject(err);
+					console.error('Error while walking notes folder for tags: ', item.path, err);
+				})
+				.on('end', async () => {
+					const resolvedFiles = (await Promise.all(files)).filter(Boolean) as {
+						path: string;
+						contents: string;
+						payload: TreeNode;
+					}[];
+					const tagIndex: { [tag: string]: TreeNode[] } = {};
+
+					for (const file of resolvedFiles) {
+						const parsedFrontMatter = this._parseFrontMatter(file);
+						if (parsedFrontMatter && parsedFrontMatter.data.tags) {
+							for (const tag of parsedFrontMatter.data.tags) {
+								if (tagIndex[tag]) {
+									tagIndex[tag].push(file.payload);
+								} else {
+									tagIndex[tag] = [file.payload];
+								}
+							}
 						}
 					}
-				}
-			}
-		});
 
-		const tags: TreeNode[] = [];
-		Object.keys(tagIndex)
-			.sort()
-			.forEach((tag) => {
-				tags.push(
-					new TreeNode(tag, vscode.TreeItemCollapsibleState.Collapsed, 'tag.svg', undefined, tagIndex[tag])
-				);
-			});
-		return tags;
+					const tags: TreeNode[] = Object.keys(tagIndex).map((tag) => ({
+						type: 'tag',
+						tag,
+						files: tagIndex[tag],
+					}));
+
+					tags.sort((a, b) => (a.tag! > b.tag! ? 1 : b.tag! > a.tag! ? -1 : 0));
+					resolve(tags);
+				});
+		});
 	}
 
-	private parseFrontMatter(file: { contents: Buffer }): matter.GrayMatterFile<string> | null {
+	private _parseFrontMatter(file: { path: string; contents: string }) {
 		try {
-			const parsedFrontMatter = matter(file.contents.toString());
-			if (!(parsedFrontMatter.data instanceof Object)) {
-				console.error('YAML front-matter is not an object');
+			const parsed = matter(file.contents);
+			if (typeof parsed.data !== 'object') {
+				console.error('YAML front-matter is not an object: ', file.path);
 				return null;
 			}
-			return parsedFrontMatter;
+			return parsed;
 		} catch (e) {
-			console.error(e);
+			console.error('Error parsing front matter:', file.path, e);
 			return null;
 		}
-	}
-}
-
-class TreeNode {
-	constructor(
-		public readonly label: string,
-		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
-		public readonly iconPath: string,
-		public readonly path?: string,
-		public readonly files: TreeNode[] = []
-	) {}
-
-	get type(): string {
-		if (this.path) {
-			return fs.statSync(this.path).isDirectory() ? 'file' : 'tag';
-		}
-		return this.label === 'Tags' ? 'rootTag' : 'rootFile';
-	}
-
-	get treeItem(): vscode.TreeItem {
-		const isDir = this.path ? fs.statSync(this.path).isDirectory() : false;
-		const state = isDir ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None;
-		const treeItem = new vscode.TreeItem(this.label, state);
-		treeItem.iconPath = {
-			light: path.join(__filename, '..', '..', 'media', 'light', this.iconPath),
-			dark: path.join(__filename, '..', '..', 'media', 'dark', this.iconPath),
-		};
-		if (!isDir) {
-			treeItem.command = {
-				command: 'vscode.open',
-				title: '',
-				arguments: [vscode.Uri.file(this.path)],
-			};
-		}
-		return treeItem;
 	}
 }
 
